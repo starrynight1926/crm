e26b8d774473550d6d2033f8919ac2b04b1d2441
starrynight <starrynight1926@gmail.com>
2026-07-21 08:04:03 +0700

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,9 @@ class DatabaseSeeder extends Seeder
             OrgStaffSeeder::class,           // Cây org + role + user + assignment (gộp)
             OrgUnitManagerSeeder::class,     // Người quản lý mỗi đơn vị
             RealDoctorsSeeder::class,        // 32 nhân sự Khối chuyên môn (3 cơ sở HN/HCM/ĐN)
+            RealCmStaffSeeder::class,        // CM sale + Team Leader + Sale (Team Giang / Team Hợi HN)
+            TeamHoiStaffSeeder::class,       // Nhân sự Team Hợi mở rộng
+            ServiceSeeder::class,            // Bảng giá dịch vụ (gói khám, lẻ...)
             DemoDataSeeder::class,           // Leads/dịch vụ/payment demo (chỉ dùng phần data ngoài nhân sự)
             TeamHoiCustomFieldSeeder::class, // Custom fields cho Team Hợi
         ]);
